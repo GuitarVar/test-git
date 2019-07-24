545e98716730b234b6730b8d121c9cf8d77556e7
Jane <1368211518@qq.com>
2019-07-24 15:31:57 +0800

    bus分支新增控制器并写入代码

---
+++ src/main/java/com/bdqn/Controller/testController.java
@@ -0,0 +1,8 @@
+package com.bdqn.Controller;
+
+public class testController {
+    public void test(){
+        System.out.println("bus添加内容");
+        System.out.println("bus向UserController添加代码");
+    }
+}
