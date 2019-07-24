0e9a3d8bb28eabc4d994108ccf37872eef0d4056
Jane <1368211518@qq.com>
2019-07-24 15:30:44 +0800

    bus分支向用户控制器UserController中添加新的代码

--- src/main/java/com/bdqn/Controller/UserController.java
+++ src/main/java/com/bdqn/Controller/UserController.java
@@ -3,5 +3,6 @@ package com.bdqn.Controller;
 public class UserController {
     public void test(){
         System.out.println("bus添加内容");
+        System.out.println("bus向UserController添加代码");
     }
 }
